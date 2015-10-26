1a886f0f3cbb991f137689fa3c6fe0442864d13b
Josh Romero <romero.josh@gmail.com>
2015-10-26 14:11:29 -0700

    Adding assignment feedback.

--- hw3/airfoil.py
+++ hw3/airfoil.py
@@ -54,9 +54,12 @@ class Airfoil:
 		""" read all the data files provided in the given directory into our Airfoil object"""
 
 		# create a list of xy coordinates
+		#--functionality_1
+		#--What if inputdir does not have a '/' at the end?! Should use os.path.join() and other modules, like you did in other methods!!!
+		#--START
 		f = open(inputdir + 'xy.dat', 'r')
 		name = f.readline().strip()
-
+		#--END
 		# read in x, y coordinates
 		for line in f:
 			self.x.append(float(line.split()[0]))
@@ -86,7 +89,9 @@ class Airfoil:
 		""" compute the chord length using the difference of maximum x coordinates and minimum x coordinates
 			an alternative way of computing chord length using Euclidean distance is given in comment
 		"""
-
+#--style_1
+#--Remove commented-out code before submission!!!
+#--START
 		xmin = min(abs(x) for x in self.x)
 #		xymin = (xmin, self.y[self.x.index(xmin)])
 		xmax = max(abs(x) for x in self.x)
@@ -94,7 +99,7 @@ class Airfoil:
 
 #		length = math.sqrt((xymin[0] - xymax[0]) ** 2 + (xymin[1] - xymax[1]) ** 2)
 		length = abs(xmax - xmin)
-
+#--END
 		return length
 
 
@@ -136,10 +141,14 @@ class Airfoil:
 					delta_cxs.append(0)
 				else:
 					delta_cxs.append((-1) * dcx)
-
+#--design_2
+#--Storing delta cx and delta cy in a list, ever growing the list. Inefficient and unnecessary.
+#--Can just have some memory allocated for a float() and increment the it by the delta cx and cy!
+#--What if the discretization of the wing was very fine? In the limit it could yield infinite datapoints. Blowing up the system....
+#--START
 		cx = sum(delta_cxs)
 		cy = sum(delta_cys)
-
+#--END
 		ct = cy * math.cos(alpha * math.pi/180) - cx * math.sin(alpha * math.pi/180)
 
 		return ct
@@ -159,10 +168,13 @@ class Airfoil:
 	def __repr__(self):
 		""" implement the representation of the airfoil object, formatted as required"""
 
+		#--style_0
+		#--Why don't you use the same column formatting for the headers??
+		#--START
 		string = "Test case: {}\n".format(self.name)
 		string += "\nalpha    cl          stagnation pt      \n"
 		string += "----- ------- --------------------------\n"
-
+#--END
 		# the required table is sorted by increasing order of alpha 
 		alphas = sorted(self.alphas)
 		for i in range(len(alphas)):
@@ -172,3 +184,15 @@ class Airfoil:
 			string += "{0:>5.2f} {1:>7.4f} ( {2:<6.4f}, {3:>7.4f}) {4:>7.4f}\n".format(alphas[i], ct, stag_x, stag_y, val)
 		
 		return string
+
+#--writeup_1
+#--Should've elaborated more on encapsulation and decomposition. In particular to OOP specific. What you mentioned is very general and is also applicable to functions...
+#--END
+
+#--functionality_6
+#--We asked to raise 3 exceptions, there are 3 missing, instead you did sys.exit() which terminates the full stack wihout useful exception object
+#--END		
+
+#--style_2
+#--Did not use git properly. not-meaningful git commit messages
+#--END
